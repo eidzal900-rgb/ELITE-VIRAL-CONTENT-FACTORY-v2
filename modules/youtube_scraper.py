3911ac3e81659dc096f07e27a7d2df7eafe15b80
eidzal900-rgb <eidzal900@gmail.com>
2026-03-05 19:00:34 +0800

    Update youtube_scraper.py

--- modules/youtube_scraper.py
+++ modules/youtube_scraper.py
@@ -1,1 +1,46 @@
+import requests
+import config
 
+SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
+VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
+
+
+def search_videos(query):
+
+    params = {
+
+        "key": config.YOUTUBE_API_KEY,
+        "part": "snippet",
+        "q": query,
+        "maxResults": config.MAX_RESULTS,
+        "type": "video",
+        "order": "date"
+
+    }
+
+    r = requests.get(SEARCH_URL, params=params)
+
+    data = r.json()
+
+    videos = []
+
+    for item in data["items"]:
+
+        videos.append(item["id"]["videoId"])
+
+    return videos
+
+
+def get_video_stats(video_ids):
+
+    params = {
+
+        "key": config.YOUTUBE_API_KEY,
+        "part": "statistics,snippet",
+        "id": ",".join(video_ids)
+
+    }
+
+    r = requests.get(VIDEO_URL, params=params)
+
+    return r.json()["items"]
