50eefa579de5ebee3f7156e594e8d62c75de8097
eidzal900-rgb <eidzal900@gmail.com>
2026-03-05 19:02:23 +0800

    Update storage.py

--- modules/storage.py
+++ modules/storage.py
@@ -1,1 +1,14 @@
+import json
+import os
+import config
 
+
+def save_json(filename, data):
+
+    os.makedirs(config.DATA_FOLDER, exist_ok=True)
+
+    path = os.path.join(config.DATA_FOLDER, filename)
+
+    with open(path, "w") as f:
+
+        json.dump(data, f, indent=2)
