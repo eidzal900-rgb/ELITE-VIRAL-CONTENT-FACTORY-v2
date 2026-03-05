32ef04064478d5b19ccccaec2b371d2864e982a1
eidzal900-rgb <eidzal900@gmail.com>
2026-03-05 19:01:28 +0800

    Update ranking_engine.py

--- modules/ranking_engine.py
+++ modules/ranking_engine.py
@@ -1,1 +1,24 @@
+from collections import defaultdict
 
+
+def rank_niches(videos):
+
+    niches = defaultdict(list)
+
+    for v in videos:
+
+        niches[v["niche"]].append(v)
+
+    ranking = {}
+
+    for niche in niches:
+
+        top = sorted(
+            niches[niche],
+            key=lambda x: x["score"],
+            reverse=True
+        )[:10]
+
+        ranking[niche] = top
+
+    return ranking
